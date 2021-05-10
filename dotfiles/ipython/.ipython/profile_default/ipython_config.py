268649feeba565132b643e05d5eb5c93004654db
Harrison Mamin <hmamin@goguardian.com>
2021-05-09 20:55:10 -0700

    del space in ipython cfg

--- dotfiles/ipython/.ipython/profile_default/ipython_config.py
+++ dotfiles/ipython/.ipython/profile_default/ipython_config.py
@@ -3,4 +3,3 @@ c.InteractiveShell.ast_node_interactivity = 'last_expr'
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
-
