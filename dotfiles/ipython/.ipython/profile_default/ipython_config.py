7daf08e24f04927b3d9f38832b56218f7fd66aea
hdmamin <hmamin2@dons.usfca.edu>
2021-05-09 20:45:11 -0700

    tmux plugins

--- dotfiles/ipython/.ipython/profile_default/ipython_config.py
+++ dotfiles/ipython/.ipython/profile_default/ipython_config.py
@@ -1,3 +1,5 @@
 c = get_config()
 c.InteractiveShell.ast_node_interactivity = 'last_expr'
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
